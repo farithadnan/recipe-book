d6fb2598c00d9f9cd761afceaa24a949498fa1f0
Farith Adnan <22324133+farithadnan@users.noreply.github.com>
2024-01-09 21:14:11 +0800

    Add comment on AppDropdown directive

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -6,6 +6,8 @@ import { Directive, ElementRef, HostBinding, HostListener } from "@angular/core"
 export class DropdownDirective {
   @HostBinding('class.open') isOpen = false;
   @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    // It checks if the clicked element is within the element to which the directive is applied to.
+    // If it is, it `toggles` the isOpen property; otherwise (click elsewhere), it sets isOpen to false.
     this.isOpen = this.elRef.nativeElement.contains(event.target) ?
     !this.isOpen : false;
   }
