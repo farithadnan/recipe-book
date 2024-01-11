f6295a2dfacc4a816f41ce223d21f45ea127896e
Farith Adnan <22324133+farithadnan@users.noreply.github.com>
2024-01-11 15:15:41 +0800

    Update recipe-detail.component.html and recipe.service.ts

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,29 @@
 import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Receipe A', 'This is a test desc', 'https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Fencrypted-tbn0.gstatic.com%2Fimages%3Fq%3Dtbn%3AANd9GcRJq2vVch6OckJUbbnPW_kxkLiHphep6OrUZ5ZM-oVjBGQfDi64GSYoqA%26s&sp=1704199690T94a86eea0abb59fcfc000c489719d5f256b81ec7033a5092e48fd709b0d79c75'),
-    new Recipe('A Test Receipe B', 'This is a test desc', 'https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Fencrypted-tbn0.gstatic.com%2Fimages%3Fq%3Dtbn%3AANd9GcRJq2vVch6OckJUbbnPW_kxkLiHphep6OrUZ5ZM-oVjBGQfDi64GSYoqA%26s&sp=1704199690T94a86eea0abb59fcfc000c489719d5f256b81ec7033a5092e48fd709b0d79c75')
+    new Recipe(
+      'Tasty Schnitzel',
+      'A super tasty Schnitzel - just awesome!',
+      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]
+    ),
+    new Recipe(
+      'Big Fat Burger',
+      'What else do you need to say?',
+      'https://upload.wikimedia.org/wikipedia/commons/1/11/Cheeseburger.png',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ]
+    )
   ];
 
   /**
